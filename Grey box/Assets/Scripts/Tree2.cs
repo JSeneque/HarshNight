9fcb33135a0a1a38924a37104caf5da69e097c69
Jean-Noel Seneque <jeannoelseneque@hotmail.com>
2018-09-23 21:51:13 +0930

    Tree Green version

---
+++ Grey box/Assets/Scripts/Tree2.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree2 : MonoBehaviour {
+    public int health = 5;
+    public int speed = 8;
+    public int logs = 4;
+    public int destroyTreeDelay = 5;
+
+    public GameObject log;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (health <= 0) {
+            rb.isKinematic = false;
+            rb.AddForce(transform.forward * speed);
+            StartCoroutine(DestroyTree(destroyTreeDelay));
+           
+        }
+	}
+
+    IEnumerator DestroyTree(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(this.gameObject);
+        Vector3 position = new Vector3(Random.Range(-1.0f, 1.0f), 0, Random.Range(-1.0f, 1.0f));
+        for (int i = 0; i < logs; i++)
+            Instantiate(log, this.gameObject.transform.position + new Vector3(0, 0, i ) + position , Quaternion.identity);
+
+    }
+
+    public void ChopMe()
+    {
+        health--;
+        if (health < 0)
+            health = 0;
+    }
+}
