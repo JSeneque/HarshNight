6f78c1fd7785d3a1458a6cd10f0e1fbac8d47907
Jean-Noel Seneque <jeannoelseneque@hotmail.com>
2018-09-26 22:32:48 +0930

    testing out shaders and highlighting

---
+++ Grey box/Assets/Scripts/HighlightController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightController : MonoBehaviour {
+
+    private HighlightObject highlightObject;
+
+    public void SelectObject(HighlightObject highlightObject)
+    {
+        if (this.highlightObject != null)
+        {
+            this.highlightObject.StopHighlight();
+        }
+        this.highlightObject = highlightObject;
+        this.highlightObject.StartHighlight();
+    }
+}
---
+++ Grey box/Assets/Scripts/HighlightObject.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class HighlightObject : MonoBehaviour {
+    public float animationTime = 1f;
+    public float threshold = 1.5f;
+
+    private HighlightController controller;
+    private Material material;
+    private Color normalColor;
+    private Color selectedColor;
+
+    private void Awake()
+    {
+        material = GetComponent<MeshRenderer>().material;
+        controller = FindObjectOfType<HighlightController>();
+
+        normalColor = material.color;
+        selectedColor = new Color(
+            Mathf.Clamp01(normalColor.r * threshold),
+            Mathf.Clamp01(normalColor.g * threshold),
+            Mathf.Clamp01(normalColor.b * threshold)
+            );
+    }
+
+    private void Start()
+    {
+        //StartHighlight();
+    }
+
+    public void StartHighlight()
+    {
+        iTween.ColorTo(gameObject, iTween.Hash(
+            "color", selectedColor,
+            "time", animationTime,
+            "easeType", iTween.EaseType.linear,
+            "loopType", iTween.LoopType.pingPong
+            ));
+    }
+
+    public void StopHighlight()
+    {
+        iTween.Stop(gameObject);
+        material.color = normalColor;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            controller.SelectObject(this);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            controller.SelectObject(this);
+        }
+    }
+}
