2c44a0c21f1fee4aa063d2ef4b1e36169c34e013
Jean-Noel Seneque <jeannoelseneque@hotmail.com>
2018-10-07 17:44:40 +1030

    Added placeholder rabbit and move away AI

---
+++ Grey box/Assets/Scripts/Rabbit.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Rabbit : MonoBehaviour {
+
+    private NavMeshAgent _agent;
+
+    public GameObject Player;
+
+    public float PlayerDistanceRun = 4.0f;
+
+	// Use this for initialization
+	void Start () {
+        _agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // get the distance from the player
+        float distance = Vector3.Distance(transform.position, Player.transform.position);
+
+        Debug.Log("Distance: " + distance);
+
+        // if the player is too close, runaway
+        if (distance < PlayerDistanceRun)
+        {
+            // work out the vector from the player to this object
+            Vector3 directionToPlayer = transform.position - Player.transform.position;
+
+            // move this object in that direction away
+            Vector3 newPos = transform.position + directionToPlayer;
+
+            _agent.SetDestination(newPos);
+        }
+	}
+}
