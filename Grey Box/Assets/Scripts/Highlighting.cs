ad4cfb839d8855c9c8c3225f5b0c45d92a97432c
Jean-Noel Seneque <jeannoelseneque@hotmail.com>
2018-11-07 21:03:08 +1030

    Added highlighting of selected objects

---
+++ Grey Box/Assets/Scripts/Highlighting.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Highlighting : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            foreach (Transform child in transform)
+            {
+                if (child.tag == "Highlight")
+                {
+                    child.gameObject.SetActive(true);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            foreach (Transform child in transform)
+            {
+                if (child.tag == "Highlight")
+                {
+                    child.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+}
