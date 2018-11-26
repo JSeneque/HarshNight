29e7747d48762c50f0d156a1170fc851e87667e1
Jean-Noel Seneque <jeannoelseneque@hotmail.com>
2018-11-26 20:29:41 +1030

    Enemy moves towards player when they are in a certain distance

---
+++ Grey Box/Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public int health;
+    public float speed = 3.0f;
+    public float pursueDistance = 5.0f;
+
+    private Transform player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+
+        // check if enemy is dead
+        if (health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void Update()
+    {
+       if (Vector3.Distance(transform.position, player.position) < pursueDistance)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
+    }
+}
