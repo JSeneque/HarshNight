719bcdcc7057248a4680bf65d969af6328bc3b82
Jean-Noel Seneque <jeannoelseneque@hotmail.com>
2018-11-26 14:37:58 +1030

    Add main player into the scene with player movement script

---
+++ Grey Box/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed = 6.0f;                                      // the speed the player will move
+    public float turnSpeed = 60.0f;                                 // the speed in which the player will turn
+    public float turnSmoothing = 15.0f;                             // used to smooth out the turning
+
+    private Vector3 movement;
+    //private Vector3 turning;
+    private Animator anim;
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        // get the componments
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        // store input axes
+        float lh = Input.GetAxisRaw("Horizontal");
+        float lv = Input.GetAxisRaw("Vertical");
+
+        Move(lh, lv);
+
+        Animating(lh, lv);
+    }
+
+    private void Animating(float lh, float lv)
+    {
+        //throw new NotImplementedException();
+
+        bool moving = lh != 0f || lv != 0f;
+
+        anim.SetBool("IsWalking", moving);
+    }
+
+    private void Move(float lh, float lv)
+    {
+        // move the player
+        movement.Set(lh, 0f, lv);
+
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        rb.MovePosition(transform.position + movement);
+
+        if (lh != 0f || lv != 0f) {
+            Rotating(lh, lv);
+        }
+    }
+
+    void Rotating(float lh, float lv)
+    {
+        Vector3 targetDirection = new Vector3(lh, 0f, lv);
+
+        Quaternion targetRotation = Quaternion.LookRotation(targetDirection, Vector3.up);
+
+        Quaternion newRotation = Quaternion.Lerp(rb.rotation, targetRotation, turnSmoothing * Time.deltaTime);
+
+        rb.MoveRotation(newRotation);
+
+    }
+
+
+}
--- Grey box/Assets/Scripts/PlayerController.cs
+++ Grey box/Assets/Scripts/PlayerController.cs
@@ -6,36 +6,61 @@ using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour
 {
-    private Animator anim;
-    public NavMeshAgent navMeshAgent;
-    public bool isWalking;
+    //private Animator anim;
+    //public NavMeshAgent navMeshAgent;
+    //public bool isWalking;
 
     //private CharacterController _controller;
 
     //public float movementSpeed;
 
+    //void Start()
+    //{
+    //    //_controller = GetComponent<CharacterController>();
+    //    anim = GetComponent<Animator>();
+    //    navMeshAgent = GetComponent<NavMeshAgent>();
+    //}
+
+    //void Update()
+    //{
+    //    //Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    //    //_controller.Move(move * Time.deltaTime * movementSpeed);
+    //    //if (move != Vector3.zero)
+    //    //    transform.forward = move;
+    //    //if(navMeshAgent.velocity != Vector3.zero)
+    //    //{
+    //    //    isWalking = true;
+    //    //} else
+    //    //{
+    //    //    isWalking = false;
+    //    //}
+
+    //    var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+    //    var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+    //    transform.Rotate(0, x, 0);
+    //    transform.Translate(0, 0, z);
+
+    //    anim.SetBool("IsWalking", isWalking);
+
+    //}
+    public float speed;
+
+    private Rigidbody rb;
+
+
     void Start()
     {
-        //_controller = GetComponent<CharacterController>();
-        anim = GetComponent<Animator>();
-        navMeshAgent = GetComponent<NavMeshAgent>();
+        rb = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        //Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        //_controller.Move(move * Time.deltaTime * movementSpeed);
-        //if (move != Vector3.zero)
-        //    transform.forward = move;
-        if(navMeshAgent.velocity != Vector3.zero)
-        {
-            isWalking = true;
-        } else
-        {
-            isWalking = false;
-        }
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
 
-        anim.SetBool("IsWalking", isWalking);
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
+        rb.AddForce(movement * speed);
     }
 }
