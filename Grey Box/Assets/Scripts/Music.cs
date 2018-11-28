5e724769848f8dcc7abd4d35249985c3d5f1ad92
Jean-Noel Seneque <jeannoelseneque@hotmail.com>
2018-11-28 14:26:24 +1030

    changed so the music is uninterrupted during scenes

---
+++ Grey Box/Assets/Scripts/Music.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour {
+
+    private static Music instance;
+
+    private void Awake()
+    {
+        // check if there is aleady an instance of the muics
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        } 
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
